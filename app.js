899760b41319c9960ce5c3331571deffdb1ee743
nexora-droid <ashlesh.deshmukh.sg@gmail.com>
2026-09-21 10:27:36 +0800

    Merge main and resolve conflicts

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
 const express = require('express');
-const router = express.router();
+const path = require('path');
+const app = express();
 
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.get('/admin', (req, res)=>{
+    res.sendFile(path.join(__dirname, 'public', 'admin.html'));
+})
+
+app.listen(4000, ()=> console.log('Server running on 4000'));
